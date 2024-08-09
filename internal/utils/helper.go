579b7f0532ba0ce833f2962046c8f8bcd6cfc6b4
yahkerobertkertasnya <robert.wiliam12345@gmail.com>
2024-08-09 15:08:44 +0700

    feat: removed godotenv

--- internal/utils/helper.go
+++ internal/utils/helper.go
@@ -1,15 +1,10 @@
 package utils
 
 import (
-	"github.com/joho/godotenv"
 	"os"
 )
 
 func GetDotENVVariable(key string, defaultVar string) string {
-	if err := godotenv.Load(); err != nil {
-		return defaultVar
-	}
-
 	variable := os.Getenv(key)
 
 	if variable == "" {
